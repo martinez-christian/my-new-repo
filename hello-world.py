fa99e7203a7a26f5f8635b7dd1ccc809aba3ea0c
martinez-christian <christianmpacheco@gmail.com>
2025-08-16 18:27:00 -0700

    hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+import random
+
+greetings = [
+  "Hello, Git!",
+  "Greetings, developer!",
+  "Welcome to branching!",
+  "Hi there, coding friend!",
+  "Happy coding!"
+]
+
+def get_random_greeting():
+  return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
